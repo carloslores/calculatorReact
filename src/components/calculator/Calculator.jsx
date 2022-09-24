cc13b7af7aa9f719d2d869074101f72b9745d20b
clorezh <clorezhh@gmail.com>
2022-09-24 14:35:43 +0200

    all operations done

--- src/components/calculator/Calculator.jsx
+++ src/components/calculator/Calculator.jsx
@@ -7,9 +7,11 @@ const Calculator = () => {
   const [newNum, setNewNum] = useState(false);
   const [isSum, setIstSum] = useState(false);
   const [isRest, setIstRest] = useState(false);
+  const [isMultiply, setIstMultiply] = useState(false);
+  const [isDivide, setIstDivide] = useState(false);
 
   const printOnScreen = (numValue) => {
-    if (!numValue) {
+    if (!numValue && numValue !== 0) {
       setActuaNum(["0"]);
     } else {
       setActuaNum(transformDataValues(numValue));
@@ -17,7 +19,9 @@ const Calculator = () => {
   };
 
   const transformDataValues = (num) => {
-    const newActualNumbber = `${actualNum.join("")}${num}`.split("");
+    const newActualNumbber = `${
+      typeof actualNum === "string" ? actualNum : actualNum.join("")
+    }${num}`.split("");
     if (newActualNumbber.length > 1 && newActualNumbber[0] == 0) {
       newActualNumbber.shift();
     }
@@ -25,27 +29,87 @@ const Calculator = () => {
   };
 
   const transformeToInt = (value) => {
-    return +value.join("");
+    let trasnformValueToCollect;
+    if (typeof value === "string") {
+      trasnformValueToCollect = value.split("");
+    } else {
+      trasnformValueToCollect = value;
+    }
+    console.log(value);
+    return +trasnformValueToCollect.join("").replace(",", ".");
   };
 
-  const sum = (callOnUseEffect) => {
-    let resultSum = null;
+  const sum = () => {
+    const resultSum = [transformeToInt(oldNum) + transformeToInt(actualNum)];
+    console.log(resultSum[0].toFixed(2));
+    return String(resultSum[0].toFixed(2)).replace(".", ",");
+  };
+
+  const rest = () => {
+    const resultRest = [transformeToInt(oldNum) - transformeToInt(actualNum)];
+    return String(resultRest[0].toFixed(2)).replace(".", ",");
+  };
+
+  const multiply = () => {
+    const resultMultiply = [
+      transformeToInt(oldNum) * transformeToInt(actualNum),
+    ];
+    return String(resultMultiply[0].toFixed(2)).replace(".", ",");
+  };
+
+  const divide = () => {
+    const resultMultiply = [
+      transformeToInt(oldNum) / transformeToInt(actualNum),
+    ];
+    return String(resultMultiply[0].toFixed(2)).replace(".", ",");
+  };
+
+  const operateNumber = (operation, callOnUseEffect) => {
     if (callOnUseEffect) {
-      console.log(oldNum);
-      console.log(actualNum);
-      const resultSum = [transformeToInt(oldNum) + transformeToInt(actualNum)];
-      setActuaNum(resultSum);
-      setNewNum(false);
-      setIstSum(false);
+      switch (operation) {
+        case "sum":
+          handleNumState(callOnUseEffect, sum);
+          setIstSum(false);
+          break;
+        case "rest":
+          handleNumState(callOnUseEffect, rest);
+          setIstRest(false);
+          break;
+        case "multiply":
+          handleNumState(callOnUseEffect, multiply);
+          setIstMultiply(false);
+          break;
+        case "divide":
+          handleNumState(callOnUseEffect, divide);
+          setIstDivide(false);
+          break;
+      }
     } else {
-      console.log(actualNum);
+      handleNumState(false);
+      switch (operation) {
+        case "sum":
+          setIstSum(true);
+          break;
+        case "rest":
+          setIstRest(true);
+          break;
+        case "multiply":
+          setIstMultiply(true);
+          break;
+        case "divide":
+          setIstDivide(true);
+          break;
+      }
+    }
+  };
+
+  const handleNumState = (callOnUseEffect, result) => {
+    if (callOnUseEffect && result) {
+      setActuaNum(result);
+      setNewNum(false);
+    } else {
       setOldNum(actualNum);
       setActuaNum(["0"]);
-      setIstSum(true);
-    }
-
-    if (resultSum) {
-      setNewNum(true);
     }
   };
 
@@ -57,8 +121,16 @@ const Calculator = () => {
       return;
     }
 
-    if (isSum && newNum) {
-      sum(true);
+    if (newNum) {
+      if (isSum) {
+        operateNumber("sum", true);
+      } else if (isRest) {
+        operateNumber("rest", true);
+      } else if (isMultiply) {
+        operateNumber("multiply", true);
+      } else if (isDivide) {
+        operateNumber("divide", true);
+      }
     }
 
     console.log("useEffect ran. count is: ", newNum);
@@ -70,90 +142,116 @@ const Calculator = () => {
         <div className="screen">{actualNum}</div>
         <div className="row">
           <div className="col">
-            <div className="btn first" onClick={() => printOnScreen(0)}>
+            <div
+              className="btn first btn-gray"
+              onClick={() => printOnScreen(null)}
+            >
               AC
             </div>
           </div>
           <div className="col">
-            <div className="btn">%</div>
-          </div>
-          <div className="col">
-            <div className="btn">÷</div>
+            <div
+              className="btn btn-yellow"
+              onClick={() => operateNumber("divide")}
+            >
+              ÷
+            </div>
           </div>
         </div>
         <div className="row">
           <div className="col">
-            <div className="btn" onClick={() => printOnScreen(7)}>
+            <div className="btn btn-dark-gray" onClick={() => printOnScreen(7)}>
               7
             </div>
           </div>
           <div className="col">
-            <div className="btn" onClick={() => printOnScreen(8)}>
+            <div className="btn btn-dark-gray" onClick={() => printOnScreen(8)}>
               8
             </div>
           </div>
           <div className="col">
-            <div className="btn" onClick={() => printOnScreen(9)}>
+            <div className="btn btn-dark-gray" onClick={() => printOnScreen(9)}>
               9
             </div>
           </div>
           <div className="col">
-            <div className="btn">X</div>
+            <div
+              className="btn btn-yellow"
+              onClick={() => operateNumber("multiply")}
+            >
+              X
+            </div>
           </div>
         </div>
         <div className="row">
           <div className="col">
-            <div className="btn" onClick={() => printOnScreen(4)}>
+            <div className="btn btn-dark-gray" onClick={() => printOnScreen(4)}>
               4
             </div>
           </div>
           <div className="col">
-            <div className="btn" onClick={() => printOnScreen(5)}>
+            <div className="btn btn-dark-gray" onClick={() => printOnScreen(5)}>
               5
             </div>
           </div>
           <div className="col">
-            <div className="btn" onClick={() => printOnScreen(6)}>
+            <div className="btn btn-dark-gray" onClick={() => printOnScreen(6)}>
               6
             </div>
           </div>
           <div className="col">
-            <div className="btn">-</div>
+            <div
+              className="btn btn-yellow"
+              onClick={() => operateNumber("rest")}
+            >
+              -
+            </div>
           </div>
         </div>
         <div className="row">
           <div className="col">
-            <div className="btn" onClick={() => printOnScreen(1)}>
+            <div className="btn btn-dark-gray" onClick={() => printOnScreen(1)}>
               1
             </div>
           </div>
           <div className="col">
-            <div className="btn" onClick={() => printOnScreen(2)}>
+            <div className="btn btn-dark-gray" onClick={() => printOnScreen(2)}>
               2
             </div>
           </div>
           <div className="col">
-            <div className="btn" onClick={() => printOnScreen(3)}>
+            <div className="btn btn-dark-gray" onClick={() => printOnScreen(3)}>
               3
             </div>
           </div>
           <div className="col">
-            <div className="btn" onClick={() => sum()}>
+            <div
+              className="btn btn-yellow"
+              onClick={() => operateNumber("sum")}
+            >
               +
             </div>
           </div>
         </div>
         <div className="row">
           <div className="col">
-            <div className="btn btn-lg" onClick={() => printOnScreen(0)}>
+            <div
+              className="btn btn-dark-gray btn-lg"
+              onClick={() => printOnScreen(0)}
+            >
               0
             </div>
           </div>
           <div className="col">
-            <div className="btn">,</div>
+            <div
+              className="btn btn-dark-gray"
+              onClick={() => printOnScreen(",")}
+            >
+              ,
+            </div>
           </div>
           <div className="col">
-            <div className="btn" onClick={() => setNewNum(true)}>
+            <div className="btn btn-yellow" onClick={() => setNewNum(true)}>
               =
             </div>
           </div>
